3883d82ca2735891bd525cf9d7125608c1df3d5e
장우진 <dnwls16071@naver.com>
2024-09-04 01:55:30 +0900

    [Feat]: 로그인 실패 핸들러 추가 및 커스텀 인증 제공 구현

--- src/main/java/project/board/config/security/SecurityConfig.java
+++ src/main/java/project/board/config/security/SecurityConfig.java
@@ -1,11 +1,8 @@
-package project.board.config;
+package project.board.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,28 +12,22 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import project.board.config.auth.PrincipalDetailsService;
+import org.springframework.stereotype.Service;
+import project.board.config.custom.CustomAuthenticationFailureHandler;
 import project.board.config.oauth.PrincipalOauthDetailsService;
 
+@Service
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final PrincipalDetailsService principalDetailsService;
+	private final CustomAuthenticationFailureHandler authenticationFailureHandler;
 	private final PrincipalOauthDetailsService principalOauthDetailsService;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(principalDetailsService);
-		authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
-		return new ProviderManager(authenticationProvider);
 	}
 
 	@Bean
@@ -48,30 +39,25 @@ public class SecurityConfig {
 		http.authorizeHttpRequests(
 				auth -> auth
 			.requestMatchers(
-				new AntPathRequestMatcher("/js/**"),
-				new AntPathRequestMatcher("/img/**"),
-				new AntPathRequestMatcher("/css/**"),
-				new AntPathRequestMatcher("/loginForm"),
-				new AntPathRequestMatcher("/signupForm"),
-				new AntPathRequestMatcher("/login"),
-				new AntPathRequestMatcher("/signup"),
-				new AntPathRequestMatcher("/"))
+				new AntPathRequestMatcher("/js/**"),			// static 하위
+				new AntPathRequestMatcher("/img/**"),			// static 하위
+				new AntPathRequestMatcher("/css/**"),			// static 하위
+				new AntPathRequestMatcher("/auth/login"),		// 로그인 폼
+				new AntPathRequestMatcher("/auth/signup"),	// 회원가입 폼
+				new AntPathRequestMatcher("/login"),			// 로그인 POST
+				new AntPathRequestMatcher("/signup"))			// 회원가입 POST
 			.permitAll().anyRequest().authenticated())
-			.formLogin(
-				formLogin -> formLogin
-			.loginPage("/loginForm")
-			.loginProcessingUrl("/login")
-			.defaultSuccessUrl("/board/posts"))
-			.logout(
-				logout -> logout.logoutRequestMatcher(
-						new AntPathRequestMatcher("/logout")
-				)
-			.logoutSuccessUrl("/board")
-			.invalidateHttpSession(true))
-			.oauth2Login(
-				oauth -> oauth.userInfoEndpoint(
+			.formLogin(formLogin -> formLogin.loginPage("/auth/login")
+						.loginProcessingUrl("/login")
+						.defaultSuccessUrl("/board")
+						.failureHandler(authenticationFailureHandler)	// 로그인 실패 핸들러
+						.permitAll())
+			.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/auth/login")
+			.invalidateHttpSession(true).deleteCookies("JSESSIONID"))
+			.oauth2Login(oauth -> oauth.userInfoEndpoint(
 				oauthService -> oauthService.userService(principalOauthDetailsService))
-			.defaultSuccessUrl("/board/posts"))
+			.defaultSuccessUrl("/board"))
 			.requestCache((cache) -> cache.requestCache(nullRequestCache));
 		return http.build();
 	}
