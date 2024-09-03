3171e37212982eb5aaf6123d0ba757c99a7c8da3
장우진 <dnwls16071@naver.com>
2024-09-04 01:37:28 +0900

    [Fix]: 회원가입용 DTO 직렬화 추가

--- src/main/java/project/board/dto/SignUpRequestDTO.java
+++ src/main/java/project/board/dto/SignUpRequestDTO.java
@@ -2,11 +2,13 @@ package project.board.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SignUpRequestDTO {
+public class SignUpRequestDTO implements Serializable {
 	private String username;
 	private String email;
 	private String password;
