7199ba9c7a2841e31a0685d44aa66ffc23ec3e9e
장우진 <dnwls16071@naver.com>
2024-09-04 02:13:28 +0900

    [Fix]: MemberRepository 전체적으로 수정

--- src/main/java/project/board/repository/MemberRepository.java
+++ src/main/java/project/board/repository/MemberRepository.java
@@ -1,12 +1,10 @@
 package project.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import project.board.model.Member;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByEmail(String email);
-	Optional<Member> findByProviderAndProviderId(@Param(value = "provider") String provider, @Param(value = "providerId") String providerId);
 }
