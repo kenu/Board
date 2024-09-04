6dc46d913329344b4ba9a79a0e74464b19ae2ace
장우진 <dnwls16071@naver.com>
2024-09-04 16:31:33 +0900

    [Feat]: 세션 관련 설정 추가

--- src/main/java/project/board/config/security/SecurityConfig.java
+++ src/main/java/project/board/config/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.NullRequestCache;
@@ -16,6 +18,10 @@ import org.springframework.stereotype.Service;
 import project.board.config.custom.CustomAuthenticationFailureHandler;
 import project.board.config.oauth.PrincipalOauthDetailsService;
 
+// Configure global AuthenticationManagerBuilder
+// Global AuthenticationManager configured with AuthenticationProvider bean with name customAuthenticationProvider
+// Global AuthenticationManager configured with an AuthenticationProvider bean. UserDetailsService beans will not be used for username/password login.
+// Consider removing the AuthenticationProvider bean. Alternatively, consider using the UserDetailsService in a manually instantiated DaoAuthenticationProvider.
 @Service
 @Configuration
 @EnableWebSecurity
@@ -28,6 +34,11 @@ public class SecurityConfig {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SessionRegistry sessionRegistry() {
+		return new SessionRegistryImpl();
 	}
 
 	@Bean
@@ -47,18 +58,25 @@ public class SecurityConfig {
 				new AntPathRequestMatcher("/login"),			// 로그인 POST
 				new AntPathRequestMatcher("/signup"))			// 회원가입 POST
 			.permitAll().anyRequest().authenticated())
+			// 폼 로그인 기반 설정
 			.formLogin(formLogin -> formLogin.loginPage("/auth/login")
 						.loginProcessingUrl("/login")
-						.defaultSuccessUrl("/board")
+						.defaultSuccessUrl("/board/posts")
 						.failureHandler(authenticationFailureHandler)	// 로그인 실패 핸들러
 						.permitAll())
+			// 로그아웃 설정
 			.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.logoutSuccessUrl("/auth/login")
 			.invalidateHttpSession(true).deleteCookies("JSESSIONID"))
+			// 소셜 로그인 설정
 			.oauth2Login(oauth -> oauth.userInfoEndpoint(
 				oauthService -> oauthService.userService(principalOauthDetailsService))
 			.defaultSuccessUrl("/board"))
 			.requestCache((cache) -> cache.requestCache(nullRequestCache));
+		// 세션 설정(
+		http.sessionManagement(session -> session.sessionFixation().changeSessionId()
+				.maximumSessions(1).maxSessionsPreventsLogin(false).sessionRegistry(sessionRegistry())
+				.expiredUrl("/duplicated-login"));
 		return http.build();
 	}
 }
